25f12c280a1bed233a9c77a722e9cffd95a149dd
Zachary D. Shaw <zdsbs@yahoo.com>
2010-05-26 21:03:24 -0400

    starting to pick the correct script files

--- db_onion.clj
+++ db_onion.clj
@@ -1,2 +1,13 @@
-(ns db-onion)
+(ns db-onion
+  (:import (java.io File FilenameFilter))
+  (:require [clojure.contrib.str-utils2 :as s]))
 
+(def script-file-filter (proxy [FilenameFilter] []
+  (accept [dir filename] 
+          (if (nil? (re-find #"[0-9]+-.*sql$" filename))
+            false
+            true))))
+
+(defn get-scripts [script-dir-path]
+    (let [onion (new File script-dir-path)]
+          (seq (.listFiles onion script-file-filter))))
--- db_onion_test.clj
+++ db_onion_test.clj
@@ -1,4 +1,16 @@
 (ns db-onion-test
-  (:use clojure.test db-onion))
+  (:use clojure.test db-onion)
+  (:import (java.io File FilenameFilter)))
+
+(def onion-test-path "db-onion/test-scripts")
+
+;(def script-file-filter (proxy [FilenameFilter] []
+;  (accept [dir filename] true)))
+
+;(doseq [file (.listFiles onion script-file-filter)] (println (.getName file)))
+
+(deftest get-scripts-test
+         (is (= (list "1-example.sql" "4-example.sql" "123-example.sql" ) (map #(.getName %) (get-scripts onion-test-path))))
+         )
 
 (run-tests 'db-onion-test)
