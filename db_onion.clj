0439e213bc1af5d4c2f7e0cf7503823c672e827a
Daniel Pierkowski <dpierkowski@gmail.com>
2010-05-26 21:18:45 -0400

    sort script files by version number

--- db_onion.clj
+++ db_onion.clj
@@ -1,13 +1,20 @@
 (ns db-onion
-  (:import (java.io File FilenameFilter))
-  (:require [clojure.contrib.str-utils2 :as s]))
+  (:import (java.io File FilenameFilter) (java.util Comparator)))
 
-(def script-file-filter (proxy [FilenameFilter] []
-  (accept [dir filename] 
+(def script-file-filter 
+  (proxy [FilenameFilter] []
+    (accept [dir filename] 
           (if (nil? (re-find #"[0-9]+-.*sql$" filename))
             false
             true))))
 
+(def numeric-sort-comparator 
+  (proxy [Comparator] []
+	(compare [this that]
+		(let [this-number (Integer/parseInt (re-find #"[0-9]+" (.getName this)))
+			  that-number (Integer/parseInt (re-find #"[0-9]+" (.getName that)))]
+			  (.compareTo this-number that-number)))))
+
 (defn get-scripts [script-dir-path]
     (let [onion (new File script-dir-path)]
-          (seq (.listFiles onion script-file-filter))))
+          (sort numeric-sort-comparator (seq (.listFiles onion script-file-filter)))))
