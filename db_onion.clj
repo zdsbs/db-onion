e356ef75ca167f60ec4acef7c25d19e6fdd6d3a1
Daniel Pierkowski <dpierkowski@gmail.com>
2010-06-01 20:09:28 -0400

    renaming some parameters for clarity.  Adds tests for script-file-filter and numeric-sort-comparator

--- db_onion.clj
+++ db_onion.clj
@@ -3,17 +3,17 @@
 
 (def script-file-filter 
   (proxy [FilenameFilter] []
-    (accept [dir filename] 
+    (accept [_ filename] 
           (if (nil? (re-find #"[0-9]+-.*sql$" filename))
             false
             true))))
 
 (def numeric-sort-comparator 
   (proxy [Comparator] []
-	(compare [this that]
-		(let [this-number (Integer/parseInt (re-find #"[0-9]+" (.getName this)))
-			  that-number (Integer/parseInt (re-find #"[0-9]+" (.getName that)))]
-			  (.compareTo this-number that-number)))))
+	(compare [file1 file2]
+		(let [file1-number (Integer/parseInt (re-find #"[0-9]+" (.getName file1)))
+			  file2-number (Integer/parseInt (re-find #"[0-9]+" (.getName file2)))]
+			  (.compareTo file1-number file2-number)))))
 
 (defn get-scripts [script-dir-path]
     (let [onion (new File script-dir-path)]
--- db_onion_test.clj
+++ db_onion_test.clj
@@ -4,10 +4,21 @@
 
 (def onion-test-path "db-onion/test-scripts")
 
-;(def script-file-filter (proxy [FilenameFilter] []
-;  (accept [dir filename] true)))
+(deftest test-numeric-sort-comparator
+	(let [file1 (File. "1-foo.sql")
+		  file2 (File. "2-foo.sql")]
+		  (is (= -1 (.compare numeric-sort-comparator file1 file2)))
+		  (is (= 1 (.compare numeric-sort-comparator file2 file1)))
+		  (is (= 0 (.compare numeric-sort-comparator file1 file1)))))
 
-;(doseq [file (.listFiles onion script-file-filter)] (println (.getName file)))
+(deftest test-script-file-filter
+	(let [dir (File. "unused")]
+		(is (.accept script-file-filter dir "1-script.sql"))
+		(is (.accept script-file-filter dir "100-script.sql"))
+		(is (not (.accept script-file-filter dir "1-script.sql.bak")))
+		(is (not (.accept script-file-filter dir "-1-script.sql")))
+		(is (not (.accept script-file-filter dir "abc-script.sql")))
+		))
 
 (deftest get-scripts-test
          (is (= (list "1-example.sql" "4-example.sql" "123-example.sql" ) (map #(.getName %) (get-scripts onion-test-path))))
