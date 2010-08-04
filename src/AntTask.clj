75b7d576b91acdb0d32e8ee2e9f4e3c78e13a42f
Daniel Pierkowski <dpierkowski@gmail.com>
2010-08-03 22:03:05 -0400

    Adding parameters to our ant task to specify DB connection info

--- src/AntTask.clj
+++ src/AntTask.clj
@@ -1,7 +1,32 @@
 (ns AntTask
   (:gen-class
-;   :extends java.lang.Exception))
-    :extends org.apache.tools.ant.Task))
+    :extends org.apache.tools.ant.Task
+    :methods [[setDriverClassName [String] void]
+              [setSubprotocol [String] void]
+              [setSubname [String] void]
+              [setUsername [String] void]
+              [setPassword [String] void]]))
 
-(defn -execute [this] (println "hello world")) 
-(defn -main [args] (println "hi in main" args))
+(def db-props (ref {}))
+
+(defn -setDriverClassName [this db-driver]
+  (dosync (alter db-props assoc :classname db-driver)))
+
+
+(defn -setSubprotocol [this subprotocol]
+(dosync (alter db-props assoc :subprotocol subprotocol)))
+
+(defn -setSubname [this subname]
+(dosync (alter db-props assoc :subname subname)))
+
+
+(defn -setUsername [this username]
+(dosync (alter db-props assoc :user username)))
+
+(defn -setPassword [this password]
+(dosync (alter db-props assoc :password password)))
+
+(defn -execute [this] 
+  (println "Executing DB-Onion with")
+  (println @db-props)) 
+
