eddda55ae99cb560694f6f7f54bee6ae3aff18df
Daniel Pierkowski <dpierkowski@gmail.com>
2010-08-09 20:20:35 -0400

    Allow specifying the script directory in the ant task

--- src/AntTask.clj
+++ src/AntTask.clj
@@ -6,13 +6,14 @@
               [setSubprotocol [String] void]
               [setSubname [String] void]
               [setUsername [String] void]
-              [setPassword [String] void]]))
+              [setPassword [String] void]
+              [setScriptPath [String] void]]))
 
 (def db-props (ref {}))
+(def script-path (ref ""))
 
 (defn -setDriverClassName [this db-driver]
   (dosync (alter db-props assoc :classname db-driver)))
-
 
 (defn -setSubprotocol [this subprotocol]
   (dosync (alter db-props assoc :subprotocol subprotocol)))
@@ -20,17 +21,23 @@
 (defn -setSubname [this subname]
   (dosync (alter db-props assoc :subname subname)))
 
-
 (defn -setUsername [this username]
   (dosync (alter db-props assoc :user username)))
 
 (defn -setPassword [this password]
   (dosync (alter db-props assoc :password password)))
 
+(defn -setScriptPath [this path]
+  (dosync (ref-set script-path path)))
+
+
+
+
+
 (defn -execute [this] 
   (println "Executing DB-Onion with")
   (dosync (ref-set db @db-props))
-  (println (run "/home/zshaw/onion-scripts"))) 
+  (println (run @script-path))) 
 
 
 
