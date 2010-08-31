9ec111dfc421f25a069b4d9266475ebb6aa14d39
Daniel Pierkowski <dpierkowski@gmail.com>
2010-08-30 20:10:27 -0400

    Upgrade to clojure 1.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject db-onion "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.h2database/h2 "1.2.138"]
                  [ant/ant "1.7.0"]]
   :aot [DBOnionScriptRunner DBOnionInitializer])
